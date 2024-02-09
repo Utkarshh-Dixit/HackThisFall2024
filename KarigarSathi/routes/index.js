fced860818477710265e2a4682c8f85662db2b72
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-02-09 20:46:55 +0530

    css for signup page and route for same

--- KarigarSathi/routes/index.js
+++ KarigarSathi/routes/index.js
@@ -10,4 +10,8 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {error: ""});
+});
+
 module.exports = router;
